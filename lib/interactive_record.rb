81b4b0bd94cf71ee82fd7a82c8cd0acb8a028ec8
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-10-05 23:59:00 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -63,10 +63,10 @@ class InteractiveRecord
       column_name = attribute.keys[0].to_s
       value_name = attribute.values[0]
 
-      sql = <<-SQL 
+      sql = <<-SQL
         SELECT * FROM #{table_name}
         WHERE #{column_name} = ?
-        SQL 
+        SQL
         DB[:conn].execute(sql, value_name);
-    end 
+    end
 end
